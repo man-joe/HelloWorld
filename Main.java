3eb4f0c7be0197cfa059d4612c5eb0fc840f9cd5
man-joe <jhoang3@gmail.com>
2019-09-04 11:23:41 -0400

    Added README.md and First Java Project

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Today is September 4, 2019. I am alive!");
+        System.out.println("My name is Joe.");
+    }
+}
